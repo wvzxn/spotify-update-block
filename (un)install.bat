d7140128d2b416e114b134eb3b6a4c3a7db3f704
wvzxn <zvp.ukr01@gmail.com>
2022-06-01 14:06:33 +0300

    initial

--- (un)install.bat
+++ (un)install.bat
@@ -1,5 +1,5 @@
 @echo off
 if not "%1"=="am_admin" ( powershell start -verb runas '%0' 'am_admin "%~1" "%~2"' & exit )
-set "_PS1_COMMAND=& %USERPROFILE%\Desktop\1.ps1"
+set "_PS1_COMMAND=[Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12; cls; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/wvzxn/spotify-update-block/master/script.ps1'))"
 start "mhddos" powershell -executionpolicy bypass -command "%_PS1_COMMAND%"
 exit
