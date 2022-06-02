e1325d758ca2ddea95e1d64f6acd6aee507d8f4a
wvzxn <zvp.ukr01@gmail.com>
2022-06-02 20:07:14 +0300

    code rewrite

--- script.ps1
+++ script.ps1
@@ -4,21 +4,17 @@ function Set-Permission {
     $AccessRule = New-Object System.Security.AccessControl.FileSystemAccessRule("$User","$Permission","ContainerInherit,ObjectInherit","None","$PermissionType")
     if ($Add) {$Acl.SetAccessRule($AccessRule); Set-Acl "$Path" $Acl} else {if ($Del) {$Acl.RemoveAccessRule($AccessRule); Set-Acl "$Path" $Acl} else {return "Error: -Add or -Del required"}}
 }
-$_sp = "$env:LOCALAPPDATA\Spotify"; $_spu = "$_sp\Update"; $_u = (Get-LocalGroupMember -Group "Administrators").Name
-if (!(Test-Path "$_sp\.update_block")) {
+$_sp = "$env:LOCALAPPDATA\Spotify"; $_spu = "$_sp\Update"
+$_u_a = ((Get-ACL "$_spu").Access | Where-Object {$_.AccessControlType -eq "Allow"}).IdentityReference.Value
+$_u_d = ((Get-ACL "$_spu").Access | Where-Object {$_.AccessControlType -eq "Deny"}).IdentityReference.Value
+if (!$_u_d) {
     Write-Output "Install Spotify update block? ` [Enter], [Y] - Yes"; $_k = [Console]::ReadKey($true).Key
     if ($_k -eq "Y" -or $_k -eq "Enter") {
         if (!(Test-Path "$_spu")) {New-Item -Path "$_sp" -Name "Update" -Type "Directory"}
-        for ($i = 0; $i -lt $_u.Count; $i++) {Set-Permission -Path "$_spu" -User $_u[$i] -Permission "FullControl" -Deny -Add}
-        New-Item -Path "$_sp" -Name ".update_block" -Type "file"
+        for ($i = 0; $i -lt $_u_a.Count; $i++) {Set-Permission -Path "$_spu" -User $_u_a[$i] -Permission "FullControl" -Deny -Add}
     }
 } else {
     Write-Output "Uninstall Spotify update block? ` [Enter], [Y] - Yes"; $_k = [Console]::ReadKey($true).Key
-    if ($_k -eq "Y" -or $_k -eq "Enter") {
-        for ($i = 0; $i -lt $_u.Count; $i++) {
-        # Set-Permission -Path "$_spu" -User $_u[$i] -Permission "FullControl" -Deny -Del
-        Get-ACL "$_sp\User Data" | Set-Acl "$_spu"; Remove-Item "$_sp\.update_block" -Force
-        }
-    }
+    if ($_k -eq "Y" -or $_k -eq "Enter") {Get-ACL "$_sp\User Data" | Set-Acl "$_spu"}
 }
 exit
