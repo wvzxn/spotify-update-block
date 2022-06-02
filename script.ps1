386dd4921bd7fccab183262943d85b1bf35d69bd
wvzxn <zvp.ukr01@gmail.com>
2022-06-02 20:20:01 +0300

    code rewrite

--- script.ps1
+++ script.ps1
@@ -1,3 +1,4 @@
+function Pause {Write-Host -NoNewline "Press any key to continue . . . "; [void][System.Console]::ReadKey($true)}
 function Set-Permission {
     param ([string]$Path,[string]$User,[string]$Permission,[switch]$Allow,[switch]$Deny,[switch]$Add,[switch]$Del)
     if ($Allow) {$PermissionType = "Allow"} else {if ($Deny) {$PermissionType = "Deny"} else {return "Error: -Allow or -Deny required"}}; $Acl = Get-Acl "$Path"
@@ -8,13 +9,14 @@ $_sp = "$env:LOCALAPPDATA\Spotify"; $_spu = "$_sp\Update"
 $_u_a = ((Get-ACL "$_spu").Access | Where-Object {$_.AccessControlType -eq "Allow"}).IdentityReference.Value
 $_u_d = ((Get-ACL "$_spu").Access | Where-Object {$_.AccessControlType -eq "Deny"}).IdentityReference.Value
 if (!$_u_d) {
-    Write-Output "Install Spotify update block? ` [Enter], [Y] - Yes"; $_k = [Console]::ReadKey($true).Key
+    Write-Output "Install Spotify Update Block? ` [Enter], [Y] - Yes"; $_k = [Console]::ReadKey($true).Key
     if ($_k -eq "Y" -or $_k -eq "Enter") {
         if (!(Test-Path "$_spu")) {New-Item -Path "$_sp" -Name "Update" -Type "Directory"}
         for ($i = 0; $i -lt $_u_a.Count; $i++) {Set-Permission -Path "$_spu" -User $_u_a[$i] -Permission "FullControl" -Deny -Add}
+        Write-Output "done!"; Pause
     }
 } else {
-    Write-Output "Uninstall Spotify update block? ` [Enter], [Y] - Yes"; $_k = [Console]::ReadKey($true).Key
-    if ($_k -eq "Y" -or $_k -eq "Enter") {Get-ACL "$_sp\User Data" | Set-Acl "$_spu"}
+    Write-Output "Uninstall Spotify Update Block? ` [Enter], [Y] - Yes"; $_k = [Console]::ReadKey($true).Key
+    if ($_k -eq "Y" -or $_k -eq "Enter") {Get-ACL "$_sp\User Data" | Set-Acl "$_spu"; Write-Output "done!"; Pause}
 }
 exit
